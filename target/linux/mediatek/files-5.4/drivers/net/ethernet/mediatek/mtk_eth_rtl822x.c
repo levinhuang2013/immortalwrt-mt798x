0e28daec8ae997c8b74d5581c3b398df2383e8dc
levinhuang2013 <152769562+levinhuang2013@users.noreply.github.com>
2024-11-25 21:57:53 +0800

    Update mtk_eth_rtl822x.c

--- target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_eth_rtl822x.c
+++ target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_eth_rtl822x.c
@@ -162,8 +162,10 @@ static int rtl822x_init(struct mtk_eth *eth, int addr)
 
 	// led0 at 10/100/1000/2.5G
 	mtk_mmd_write(eth, addr, 31, 0xd032, 0x0027);
-	// led on time = 400ms, duty = 12.5%, freq = 60ms, Enable 10M LPI, modeA, act
-	mtk_mmd_write(eth, addr, 31, 0xd040, 0x321f);
+	// led1 at 1000/2.5G/2.5G lite
+	mtk_mmd_write(eth, addr, 31, 0xd034, 0x00a4);
+	// led on time = 400ms, duty = 12.5%, freq = 60ms, Enable 10M LPI, modeA, LED0 act on, LED1 act off
+	mtk_mmd_write(eth, addr, 31, 0xd040, 0x321d);
 	// all led enable, polar = low
 	mtk_mmd_write(eth, addr, 31, 0xd044, 0xf8);
 
