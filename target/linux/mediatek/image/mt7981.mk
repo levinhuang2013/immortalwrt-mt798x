ace008f4c853c8e275ec50afe53fce8966e25b5a
levinhuang2013 <152769562+levinhuang2013@users.noreply.github.com>
2024-12-01 20:41:38 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -70,7 +70,7 @@ define Device/sx-7981r128
   DEVICE_MODEL := 7981R128
   DEVICE_DTS := mt7981-spim-nand-7981r128
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4 kmod-fs-f2fs kmod-fs-vfat kmod-fs-ext4 kmod-sfp
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4 kmod-fs-f2fs kmod-sfp
   SUPPORTED_DEVICES := sx,7981r128
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
