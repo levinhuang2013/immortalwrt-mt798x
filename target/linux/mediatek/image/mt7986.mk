488e7a62dc82781ce4ee3236614780903247e96f
levinhuang2013 <152769562+levinhuang2013@users.noreply.github.com>
2024-12-02 12:11:50 +0800

    Update mt7986.mk

--- target/linux/mediatek/image/mt7986.mk
+++ target/linux/mediatek/image/mt7986.mk
@@ -496,9 +496,8 @@ define Device/lunzn_s20p
   DEVICE_DTS := mt7986a-lunzn-s20p
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   SUPPORTED_DEVICES := lunzn,s20p
-  DEVICE_PACKAGES := $(MT7986_USB_PKGS) $(MT7986_WWAN_PKGS) mkf2fs e2fsprogs losetup \
-		     mmc-utils f2fsck f2fs-tools kmod-nvme kmod-mmc kmod-fs-f2fs \
-		     kmod-nls-iso8859-1 kmod-scsi-generic kmod-ata-ahci pciutils kmod-nls-cp437
+  DEVICE_PACKAGES := $(MT7986_USB_PKGS) $(MT7986_WWAN_PKGS) mkf2fs e2fsprogs losetup mmc-utils f2fsck \
+		     f2fs-tools kmod-nvme kmod-mmc kmod-fs-f2fs kmod-scsi-generic kmod-ata-ahci pciutils
   IMAGE/factory.bin := append-kernel | pad-to 32M | append-rootfs
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
