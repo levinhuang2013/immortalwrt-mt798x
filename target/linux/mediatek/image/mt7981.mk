dc8ccaf0dea0dd8eacc38f777341a199a642c09d
levinhuang2013 <152769562+levinhuang2013@users.noreply.github.com>
2024-12-22 15:51:14 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -177,16 +177,27 @@ define Device/mt7981-fpga-sd
 endef
 TARGET_DEVICES += mt7981-fpga-sd
 
-define Device/sl_3000
+define Device/sl_3000-nor-emmc
   DEVICE_VENDOR := SL
-  DEVICE_MODEL := 3000
-  DEVICE_DTS := mt7981-sl-3000-emmc-nor
+  DEVICE_MODEL := 3000-NOR
+  DEVICE_DTS := mt7981-sl-3000-nor-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := sl,3000
+  SUPPORTED_DEVICES := sl,3000-nor
   DEVICE_PACKAGES := f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += sl_3000
+TARGET_DEVICES += sl_3000-nor-emmc
+
+define Device/sl_3000-emmc
+  DEVICE_VENDOR := SL
+  DEVICE_MODEL := 3000-eMMC
+  DEVICE_DTS := mt7981-sl-3000-emmc
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := sl,3000-emmc
+  DEVICE_PACKAGES := f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += sl_3000-emmc
 
 define Device/mt7981-360-t7
   DEVICE_VENDOR := MediaTek
