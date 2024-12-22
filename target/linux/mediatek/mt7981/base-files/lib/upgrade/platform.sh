670b2ddeda15ecdbb5c028cf83a3ccee61c5e50b
levinhuang2013 <152769562+levinhuang2013@users.noreply.github.com>
2024-12-22 16:29:23 +0800

    Update platform.sh

--- target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
@@ -219,6 +219,8 @@ platform_do_upgrade() {
 		;;
 	cmcc,rax3000m-emmc |\
 	cmcc,xr30-emmc |\
+  	sl,3000-nor |\
+ 	sl,3000-emmc |\
 	*emmc*)
 		CI_KERNPART="kernel"
 		CI_ROOTPART="rootfs"
@@ -269,6 +271,7 @@ platform_check_image() {
 	*newland,nl-wr8103* |\
 	newland,nl-wr9103 |\
 	nradio,wt9103 |\
+ 	sl,3000* |\
 	*snand* |\
 	*emmc* |\
 	routerich,ax3000)
