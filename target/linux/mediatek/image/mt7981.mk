d44b3d296d97ad31ce2d939621bb15de4777a8dc
levinhuang2013 <152769562+levinhuang2013@users.noreply.github.com>
2025-02-11 16:59:24 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -1,6 +1,6 @@
 KERNEL_LOADADDR := 0x48080000
 
-MT7981_USB_PKGS := automount blkid blockdev fdisk kmod-nls-cp437 kmod-nls-iso8859-1 kmod-usb2 kmod-usb3 kmod-usb-net-rndis usbutils
+MT7981_USB_PKGS := automount blkid blockdev fdisk kmod-nls-cp437 kmod-nls-iso8859-1 kmod-usb2 kmod-usb3 usbutils
 
 define Device/mt7981-spim-nor-rfb
   DEVICE_VENDOR := MediaTek
